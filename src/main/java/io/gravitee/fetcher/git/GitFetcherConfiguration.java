834fef127dec8ff00fe4e3bca301f308741d9a06
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2024-06-04 09:21:22 +0200

    refactor: use lombok

--- src/main/java/io/gravitee/fetcher/git/GitFetcherConfiguration.java
+++ src/main/java/io/gravitee/fetcher/git/GitFetcherConfiguration.java
@@ -16,11 +16,15 @@
 package io.gravitee.fetcher.git;
 
 import io.gravitee.fetcher.api.FetcherConfiguration;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Nicolas GERAUD (nicolas <AT> graviteesource.com)
  * @author GraviteeSource Team
  */
+@Getter
+@Setter
 public class GitFetcherConfiguration implements FetcherConfiguration {
 
     private String repository;
@@ -32,46 +36,4 @@ public class GitFetcherConfiguration implements FetcherConfiguration {
     private String fetchCron;
 
     private boolean autoFetch = false;
-
-    @Override
-    public String getFetchCron() {
-        return fetchCron;
-    }
-
-    public void setFetchCron(String fetchCron) {
-        this.fetchCron = fetchCron;
-    }
-
-    @Override
-    public boolean isAutoFetch() {
-        return autoFetch;
-    }
-
-    public void setAutoFetch(boolean autoFetch) {
-        this.autoFetch = autoFetch;
-    }
-
-    public String getRepository() {
-        return repository;
-    }
-
-    public void setRepository(String repository) {
-        this.repository = repository;
-    }
-
-    public String getBranchOrTag() {
-        return branchOrTag;
-    }
-
-    public void setBranchOrTag(String branchOrTag) {
-        this.branchOrTag = branchOrTag;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
 }
